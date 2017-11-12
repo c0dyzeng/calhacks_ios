a581b0710524150b94de3c12e07d39bd1274c932
ellefcheung <ellefcheung@gmail.com>
2017-11-12 13:59:32 -0800

    added mapview

--- Beacon/Beacon/FirstViewController.swift
+++ Beacon/Beacon/FirstViewController.swift
@@ -7,12 +7,41 @@
 //
 
 import UIKit
+import MapKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    let regionRadius: CLLocationDistance = 1000
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // hardcode initial location
+        // TODO later: instead access user location, center there
+        let initialLoc = CLLocation(latitude: 37.8722, longitude: -122.2596)
+        centerMapOnLocation(location: initialLoc)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = CLLocationCoordinate2D(
+                latitude: 37.8756,
+                longitude: -122.2588)
+        annotation.title = "Soda Hall"
+        annotation.subtitle = "eecs"
+        mapView.addAnnotation(annotation)
+        
+        // TODO : figure out how to customize pins
+//        let annotationView = MKPinAnnotationView()
+//        annotationView.pinTint
+        
+    }
+    
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion =
+            MKCoordinateRegionMakeWithDistance(
+                location.coordinate,
+                regionRadius * 2.0,
+                regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
